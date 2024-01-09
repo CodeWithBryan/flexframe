47831fa3e2a2c4255c91cbb0358e0a188f50a4c4
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-01-09 15:06:56 +0500

    tailwind config customize

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <></>;
+  return <h1 className=''>inter text</h1>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      screens: {
+        xs: '325px',
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1280px',
+      },
+      fontSize: {
+        // Text series
+        't-xs': ['.75rem', '1rem'],
+        't-sm': ['.875rem', '1.25rem'],
+        't-md': ['1rem', '1.5rem'],
+        't-lg': ['1.125rem', '1.75rem'],
+        't-xl': ['1.25rem', '1.875rem'],
+
+        // Display series
+        'd-xs': ['1.5rem', '2rem'],
+        'd-sm': ['1.875rem', '2.375rem'],
+        'd-md': ['2.25rem', '2.75rem'],
+        'd-lg': ['3rem', '3.75rem'],
+        'd-xl': ['3.75rem', '4.5rem'],
+        'd-2xl': ['4.5rem', '5.5rem'],
+      },
+      fontWeight: {
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
+      },
+    },
   },
   plugins: [],
 };
