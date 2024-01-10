9f5b62a0be1d760845202488ebaf1230fc3f004a
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-01-10 23:28:47 +0500

    typography components added

--- src/components/shared/Typography/Typography.tsx
+++ src/components/shared/Typography/Typography.tsx
@@ -9,8 +9,29 @@ export const smallText = ({ children, styles = 'text-black-main' }: Props) => {
   return <p className={`text-t-sm font-medium ${styles}`}>{children}</p>;
 };
 
+export const mediumText = ({
+  children,
+  styles = 'text-black-main font-semibold',
+}: Props) => {
+  return <p className={`text-t-md ${styles}`}>{children}</p>;
+};
+
+export const text = ({
+  children,
+  styles = 'text-black-main font-semibold',
+}: Props) => {
+  return <p className={`text-t-lg ${styles}`}>{children}</p>;
+};
+
+export const Heading1 = ({ children, styles = 'text-black-main' }: Props) => {
+  return <p className={`text-d-2xl font-bold ${styles}`}>{children}</p>;
+};
+
 const Typography = {
   smallText: smallText,
+  mText: mediumText,
+  h1: Heading1,
+  text: text,
 };
 
 export default Typography;
