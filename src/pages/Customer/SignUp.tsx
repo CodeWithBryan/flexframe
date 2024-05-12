a645c217f77ccdfab0bf81d9dc0a86d82a8ec9fc
Bryan Gillespie <bryan@onebrief.com>
2024-05-12 11:06:23 -0400

    title update

--- src/pages/Customer/SignUp.tsx
+++ src/pages/Customer/SignUp.tsx
@@ -40,7 +40,7 @@ const SignUp: React.FC = () => {
             Login to unlock a world of fitness tailored just for you.
           </Typography.mText>
           {/* sign In with ---->  */}
-          <div className='w-full py-3 grid grid-cols-3 gap-2'>
+          {/* <div className='w-full py-3 grid grid-cols-3 gap-2'>
             {signWith.map((item, index) => {
               return (
                 <Link
@@ -52,15 +52,15 @@ const SignUp: React.FC = () => {
                 </Link>
               );
             })}
-          </div>
+          </div> */}
           {/* or ----->  */}
-          <div className='w-full grid grid-cols-[1fr,20px,1fr] justify-center items-center gap-2'>
+          {/* <div className='w-full grid grid-cols-[1fr,20px,1fr] justify-center items-center gap-2'>
             <div className='w-full h-[1px] bg-white-3'></div>
             <p className='text-[11px] font-inter font-medium text-[#868C98]'>
               OR
             </p>
             <div className='w-full h-[1px] bg-white-3'></div>
-          </div>
+          </div> */}
           {/* inputs ----->  */}
           <div className='flex flex-col gap-3 mt-2 sm:mt-4 items-start w-full'>
             {/* full name ----->  */}
@@ -165,19 +165,19 @@ const SignUp: React.FC = () => {
   );
 };
 
-const signWith = [
-  {
-    icon: <Icons.apple className='w-[20px] h-[21px]' />,
-    path: '#',
-  },
-  {
-    icon: <Icons.faceBook3 className='w-[20px] h-[21px]' />,
-    path: '#',
-  },
-  {
-    icon: <Icons.google className='w-[20px] h-[21px]' />,
-    path: '#',
-  },
-];
+// const signWith = [
+//   {
+//     icon: <Icons.apple className='w-[20px] h-[21px]' />,
+//     path: '#',
+//   },
+//   {
+//     icon: <Icons.faceBook3 className='w-[20px] h-[21px]' />,
+//     path: '#',
+//   },
+//   {
+//     icon: <Icons.google className='w-[20px] h-[21px]' />,
+//     path: '#',
+//   },
+// ];
 
 export default SignUp;
